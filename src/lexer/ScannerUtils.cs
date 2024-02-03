7d614c9b7d99eec95f83c7b40fd57b437d75ec15
thegman254 <gabrielplayroom@gmail.com>
2024-02-03 22:44:38 +0000

    Different implementation

--- src/lexer/ScannerUtils.cs
+++ src/lexer/ScannerUtils.cs
@@ -1,6 +1,5 @@
 namespace csteapot.lexer;
 
 static class ScannerUtils {
-    public static bool IsDigit(char c) => c == '0' || c == '1' || c == '2' || c == '3' || c == '4' ||
-                                          c == '5' || c == '6' || c == '7' || c == '8' || c == '9';
+    public static bool IsDigit(char c) => int.TryParse(c.ToString(), out var success);
 }
